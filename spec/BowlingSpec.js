2e23901ab76b8da7a3c5d391d063c36d7387d4e5
AaronRodrigues <aaronrod@gmail.com>
2017-10-08 20:29:02 +0100

    Can bowl a spare

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -1,15 +1,32 @@
 describe('The Bowling Game', function(){
   
-  it('exists',function(){
-    var game = new BowlingGame();
-    expect(game).toBeTruthy();
+  var game;
+  
+  beforeEach(function(){
+    game = new BowlingGame();    
   });
   
   it('can roll a gutter game', function(){
-    var game = new BowlingGame();
-     for (var i = 0; i < 20; i++) {
-      game.roll(0); 
-         }
+    rollMany(0,20);
     expect(game.score()).toBe(0);
   });
+  
+  it('can roll all ones', function(){
+    rollMany(1,20);
+    expect(game.score()).toBe(20);
+  });
+  
+  it('can roll a spare', function(){
+    game.roll(6);
+    game.roll(4);
+    game.roll(3);
+    rollMany(0,17);
+    expect(game.score()).toBe(16);
+  });
+  
+  var rollMany = function(pins, rolls){
+    for (var i = 0; i < rolls; i++) {
+      game.roll(pins);
+    }
+  };
 });
